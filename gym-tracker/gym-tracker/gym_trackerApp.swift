3d9c9babd11d1da42b78f5b952e4c77b3f251fc9
filipkuser <filipkuser2003@gmail.com>
2025-11-11 17:55:23 +0100

    inital commit

---
+++ gym-tracker/gym-tracker/gym_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gym_trackerApp.swift
+//  gym-tracker
+//
+//  Created by Filip Kušer on 11.11.2025..
+//
+
+import SwiftUI
+
+@main
+struct gym_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
